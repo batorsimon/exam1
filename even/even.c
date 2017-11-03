e1a24a40737dc1b4543a5e2059e7b9c6229bc2a1
batorsimon <bator.simon8@gmail.com>
2017-11-03 10:24:27 +0100

    task done

--- even/even.c
+++ even/even.c
@@ -10,7 +10,25 @@
  * Non-positive "N" numbers should be treated as errors, handle it!
  */
 
-int main() {
+int printing(int N)
+{
+    if(N < 0){
+        printf("Please do not give negative number as a parameter!\n");
+        return -1;
+    }
 
-  return 0;
+    printf("The first %d positive even numbers are: ", N);
+    int i = 0;
+    for(i = 0; i < N; i++){
+        if(i % 2 == 0){
+            printf("%d ", i);
+            N++;
+        }
+    }
+}
+
+int main()
+{
+    printing(5);
+    return 0;
 }
