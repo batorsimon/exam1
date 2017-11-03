1237bca3542a63ed90efc90cb7c14ef1565c4648
batorsimon <bator.simon8@gmail.com>
2017-11-03 11:33:52 +0100

    function switches and reverse the name

--- reverse/reverse.c
+++ reverse/reverse.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 /* Create a function that can reverse a string (char array)
  * It should take the string as a parameter and prints out
@@ -14,34 +15,62 @@
 
 void reversing(char *arr, char *reversed)
 {
-    char temp[50];
-    printf("Original name: %s\n\n", arr);
+    char first_part[50];
+    printf("Original name: %s\n", arr);
     int i = 0;
     for(i = 0; arr[i] != 32 ; i++) {
-        temp[i] = arr[i];
+        first_part[i] = arr[i];
     }
 
     int j = 0;
+    char second_part[50];
     for(i = i+1; arr[i] != '\0'; i++) {
-        reversed[j] = arr[i];
+        second_part[j] = arr[i];
         j++;
     }
-    reversed[j] = ' ';
-    reversed[j+1] = '\0';
+    second_part[j] = ' ';
+    second_part[j+1] = '\0';
 
-    for(i = 0; reversed[i] != '\0'; i++);  //stores the length of reversed name
 
-    for(j = 0; temp[j] != '\0'; j++, i++)
+    char temp[50];
+    for(i = 0; first_part[i] != '\0'; i++);
+    int a = 0;
+    for(j = i-1; j >= 0; j--){
+        temp[a] = first_part[j];
+        a++;
+    }
+    temp[a] = '\0';
+
+    char temp2[50];
+    for(i = 0; second_part[i] != '\0'; i++);
+    a = 0;
+    for(j = i-1; j >= 0; j--){
+        temp2[a] = second_part[j];
+        a++;
+    }
+    temp2[a] = '\0';
+    //printf("The name switched and reversed: %s%s\n", temp, temp2);
+
+    for(i = 0; temp[i] != '\0'; i++)
     {
-        reversed[i] = temp[j];
+        reversed[i] = temp[i];
+    }
+
+    a = 0;
+    for(j = i-1; temp2[a] != '\0'; i++)
+    {
+        reversed[j] = temp[a];
+        a++;
     }
     reversed[i] = '\0';
-    printf("The name reversed: %s\n", reversed);
+    printf("The name switched and reversed: %s\n", reversed);
+
 }
 
 int main()
 {
     char arr[] = "Green Fox";
+    //gets(arr);
     char reversed[50];
     reversing(arr, reversed);
 
